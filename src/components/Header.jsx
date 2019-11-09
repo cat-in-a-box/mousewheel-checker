2118f4957bceffb668b8eed766af41d2b0203b3f
cat-in-a-box <timoshuk.alexander@gmail.com>
2019-11-09 06:23:56 +0200

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="navbar">
-      <div className="navbarButton1"><a href="#section1">Back to the home page</a></div>
+      <div className="navbarButton1"><a href="#section1">Back to home page</a></div>
     </div>
   )
 };
