4f5961847dd70bd46878149dd5aca44257309098
cat-in-a-box <timoshuk.alexander@gmail.com>
2019-11-19 08:37:10 +0200

    refresh speed update

--- src/components/WheelCheckRotation.jsx
+++ src/components/WheelCheckRotation.jsx
@@ -4,23 +4,23 @@ export default class WheelCheckRotation extends React.Component {
 
     componentDidMount() {
 
-        let mouseArea = document.querySelector('.WheelCheckArea');
+        let mouseArea = document.querySelector ('.WheelCheckArea');
         let mouseUpCooldown;
         let mouseDownCooldown;
 
-        mouseArea.addEventListener('wheel', function (a) {
+        mouseArea.addEventListener ('wheel', function (a) {
             if (a.deltaY > 0) {
                 document.getElementById('WheelDown').classList.add('active');
                 window.clearTimeout(mouseDownCooldown);
                 mouseDownCooldown = window.setTimeout(function () {
                     document.getElementById('WheelDown').classList.remove('active')
-                }, 400);
+                }, 175);
             } else if (a.deltaY < 0) {
                 document.getElementById('WheelUp').classList.add('active');
                 window.clearTimeout(mouseUpCooldown);
                 mouseUpCooldown = window.setTimeout(function () {
                     document.getElementById('WheelUp').classList.remove('active')
-                }, 400);
+                }, 175);
             }
 
         });
