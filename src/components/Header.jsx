b83a26903bf9a23a4b3e5ed6d1a731db9b21ad54
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-05 10:29:42 +0200

    Header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return (
-    <div className="navBar">
-      <div><a href="https://cat-in-a-box.github.io/webpage/">🠠 Back to home page</a></div>
-        <div className="navBarText">Just move your mouse at the mouse and try scrolling your wheel</div>
-    </div>
-  )
+    return (
+        <div className="Header">
+            <a href="https://cat-in-a-box.github.io/webpage/">🠠 Back to home page</a>
+        </div>
+    )
 };
 
 export default Header;
