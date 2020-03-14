01bed4901e1d8ee8d76b07f7703c19ca71b9fca0
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-14 16:47:21 +0200

    Removed Helmet package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./App.css";
-// Установил и импортировал Helmet, с помощью которого изменил page title.
-// Насколько я понял, это самый простой способ менять его (как и атрибуты страницы) на реакте.
-// А еще подгружает скрипты. Нормально.
-// Поставил еще React-Scrollable-Anchor для красивых якорей и плавного перемещения
-import { Helmet } from "react-helmet";
+// Поставил React-Scrollable-Anchor для красивых якорей и плавного перемещения
 import Header from './components/Header.jsx';
 import WheelCheckArea from "./components/WheelCheckArea.jsx";
 import WheelCheckRotation from "./components/WheelCheckRotation.jsx";
@@ -12,10 +8,6 @@ import WheelCheckRotation from "./components/WheelCheckRotation.jsx";
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>mousewheel_checker</title>
-        <meta charSet="utf-8"/>
-      </Helmet>
       <Header/>
       <WheelCheckArea/>
       <WheelCheckRotation/>
