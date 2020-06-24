f0e39713e653235f1143c30cc9817909ea266639
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-06-24 06:24:50 +0300

    Language change

---
+++ src/components/Description.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Description = () => {
+    return (
+        <div className="Description">Просто крутите колесо, и мышь снизу покажет вам, всё ли у вас работает</div>
+    )
+};
+
+export default Description;
